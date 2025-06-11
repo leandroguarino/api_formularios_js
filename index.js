b0cae6ac926948cdd4da37073abef2b838af23a6
Leandro Guarino <le.guarino@gmail.com>
2025-06-10 22:24:50 -0300

    feat: cadastro de produtos

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
+app.use(cors())
+app.use(express.json()) //ensina o back a ler json
 
 let produtos = [
     { id: 1, nome: 'Produto 1', preco: 10.00, descricao: 'Descrição do Produto 1' },
@@ -8,11 +11,15 @@ let produtos = [
     { id: 3, nome: 'Produto 3', preco: 30.00, descricao: 'Descrição do Produto 3' }
 ]
 
-app.post('/clientes', (req, res) => {
-
+app.post('/produtos', (req, res) => {
+  let novoProduto = req.body
+  produtos.push(novoProduto)
+  res.json({
+    success: true
+  })
 })
 
-app.get('/clientes', (req, res) => {
+app.get('/produtos', (req, res) => {
     res.json(produtos)
 })
 
